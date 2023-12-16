0f43f4d0236024f324a81d8799b64c30914abc28
采庶子 <64427054+Gfssfa@users.noreply.github.com>
2023-12-16 11:30:27 +0800

    Add files via upload

---
+++ add_wm_mpi.py
@@ -0,0 +1,250 @@
+import cv2
+import numpy as np
+import time
+from mpi4py import MPI
+from wm_utils import dct_2d, idct_2d, logistic_map_encryption
+
+# 初始化一些变量
+alpha = 0.1
+block_size = 8
+mask = np.array([
+    [0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+# 计算mask中填充位数
+fill_cnt = np.sum(mask)
+
+# 加载并处理水印图像
+watermark_image_path = 'seu_logo_bw.jpg'
+watermark_image = cv2.imread(watermark_image_path, cv2.IMREAD_GRAYSCALE)
+watermark_image = cv2.resize(watermark_image, (256, 256), interpolation=cv2.INTER_AREA)
+encrypted_watermark = logistic_map_encryption(watermark_image, key=0.5)
+encrypted_watermark_flat = encrypted_watermark.flatten()
+
+# 补0使长度为11的倍数
+padding_length = fill_cnt - (len(encrypted_watermark_flat) % fill_cnt)
+encrypted_watermark_flat = np.pad(encrypted_watermark_flat, (0, padding_length), 'constant')
+
+# 加载宿主图像
+host_image_path = 'frame.jpg'
+bgr_image = cv2.imread(host_image_path)
+yuv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2YUV)
+Y, U, V = cv2.split(yuv_image)
+
+Y_shape_0=Y.shape[0]
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+np.set_printoptions(threshold=np.inf)
+
+if rank == 0:
+    start_time = time.time()
+
+    # # 初始化一些变量
+    # alpha = 0.1
+    # block_size = 8
+    # mask = np.array([
+    #     [0, 0, 0, 0, 1, 1, 0, 0],
+    #     [0, 0, 0, 1, 1, 0, 0, 0],
+    #     [0, 0, 1, 1, 0, 0, 0, 0],
+    #     [0, 1, 1, 0, 0, 0, 0, 0],
+    #     [1, 1, 0, 0, 0, 0, 0, 0],
+    #     [1, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 0, 0]
+    # ])
+
+    # # 计算mask中填充位数
+    # fill_cnt = np.sum(mask)
+
+    # 加载宿主图像
+    host_image_path = 'frame.jpg'
+    bgr_image = cv2.imread(host_image_path)
+    yuv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2YUV)
+    Y, U, V = cv2.split(yuv_image)
+
+    Y_shape_0=Y.shape[0]
+
+    # # 加载并处理水印图像
+    # watermark_image_path = 'seu_logo_bw.jpg'
+    # watermark_image = cv2.imread(watermark_image_path, cv2.IMREAD_GRAYSCALE)
+    # watermark_image = cv2.resize(watermark_image, (256, 256), interpolation=cv2.INTER_AREA)
+    # encrypted_watermark = logistic_map_encryption(watermark_image, key=0.5)
+    # encrypted_watermark_flat = encrypted_watermark.flatten()
+
+    # # 补0使长度为11的倍数
+    # padding_length = fill_cnt - (len(encrypted_watermark_flat) % fill_cnt)
+    # encrypted_watermark_flat = np.pad(encrypted_watermark_flat, (0, padding_length), 'constant')
+
+
+else:
+    # alpha = None
+    # block_size = None
+    # fill_cnt = None
+    # mask = None
+    # encrypted_watermark_flat = None
+    Y_shape_0 = None
+
+# 广播一些共同的变量
+# alpha = comm.bcast(alpha, root=0)
+# block_size = comm.bcast(block_size, root=0)
+# fill_cnt = comm.bcast(fill_cnt, root=0)
+# mask = comm.bcast(mask, root=0)
+# encrypted_watermark_flat = comm.bcast(encrypted_watermark_flat, root=0)
+Y_shape_0 = comm.bcast(Y_shape_0, root=0)
+
+if rank == 0:
+    # 主进程分发块
+    start_part_time=time.time()
+    for i in range(0, Y.shape[0], block_size):
+        for j in range(0, Y.shape[1], block_size):
+            block = Y[i:i+block_size, j:j+block_size]
+            dest_rank = (i // block_size) * (Y.shape[1] // block_size) + (j // block_size)
+            #dest_rank = (i // block_size) * (Y.shape[0] // block_size) + (j // block_size)
+            dest_rank = dest_rank % (size - 1) + 1
+            comm.send((block, i, j), dest=dest_rank)
+
+    # 主进程收集处理后的块
+    for _ in range((Y.shape[0] // block_size) * (Y.shape[1] // block_size)):
+        processed_block, i, j = comm.recv(source=MPI.ANY_SOURCE)
+        Y[i:i+block_size, j:j+block_size] = processed_block
+
+    # 发送结束信号给所有工作进程
+    for r in range(1, size):
+        comm.send((None, None, None), dest=r)
+
+else:
+    # 工作进程处理块
+    while True:
+        block, i, j = comm.recv(source=0)
+        if block is None:  # 收到结束信号
+            break
+
+        index = (((i // block_size) * (Y_shape_0// block_size) + (j // block_size)) * fill_cnt) % len(encrypted_watermark_flat)
+        dct_block = dct_2d(block)
+        for x in range(block_size):
+            for y in range(block_size):
+                if mask[x, y] == 1 and index < len(encrypted_watermark_flat):
+                    dct_block[x, y] += alpha * encrypted_watermark_flat[index]
+                    index += 1
+        idct_block = idct_2d(dct_block)
+        comm.send((idct_block, i, j), dest=0)
+
+
+if rank == 0:
+    end_part_time=time.time()
+    print("分块处理运行时间：", end_part_time - start_part_time, "秒")
+    # 合并处理后的Y通道和原始的U、V通道，并保存含水印图像
+    merged_yuv_image = cv2.merge([Y, U, V])
+    reconstructed_bgr_image = cv2.cvtColor(merged_yuv_image, cv2.COLOR_YUV2BGR)
+    cv2.imwrite('embedded_image_mpi.jpg', reconstructed_bgr_image)
+
+    # # 计算并保存差异图像
+    # difference_image = cv2.absdiff(bgr_image, reconstructed_bgr_image)
+    # cv2.imwrite('difference_image.jpg', difference_image)
+
+    # 打印总运行时间
+    total_time = time.time() - start_time
+    print("总运行时间：", total_time, "秒")
+
+
+
+
+
+'''
+import time
+import cv2
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
+from wm_utils import dct_2d, idct_2d, logistic_map_encryption
+from mpi4py import MPI
+
+np.set_printoptions(threshold=np.inf)
+
+# 记录程序开始时间
+start_time = time.time()
+
+alpha = 0.05 # 水印添加强度
+block_size = 8
+
+# 定义掩码矩阵
+mask = np.array([
+    [0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+
+# 加载宿主图像
+host_image_path = 'frame.jpg'  # 替换为宿主图像路径
+bgr_image = cv2.imread(host_image_path)
+yuv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2YUV)  # 修正颜色转换
+Y, U, V = cv2.split(yuv_image)
+
+# 加载并处理水印图像
+watermark_image_path = 'seu_logo_bw.jpg'  # 替换为水印图像路径
+watermark_image = cv2.imread(watermark_image_path, cv2.IMREAD_GRAYSCALE)
+watermark_image = cv2.resize(watermark_image, (256, 256), interpolation=cv2.INTER_AREA)
+encrypted_watermark = logistic_map_encryption(watermark_image, key=0.5)
+encrypted_watermark_flat = encrypted_watermark.flatten()
+
+# 补0使长度为11的倍数
+padding_length = 11 - (len(encrypted_watermark_flat) % 11)
+encrypted_watermark_flat = np.pad(encrypted_watermark_flat, (0, padding_length), 'constant')
+
+end_time_part1 = time.time()
+print("第一部分运行时间：", end_time_part1 - start_time, "秒")
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+
+
+index = 0
+
+for i in range(0, Y.shape[0], block_size):
+    for j in range(0, Y.shape[1], block_size):
+        block = Y[i:i+block_size, j:j+block_size]
+        dct_block = dct_2d(block) #DCT变换
+
+        for x in range(block_size):
+            for y in range(block_size):
+                if mask[x, y] == 1:
+                    dct_block[x, y] += alpha * encrypted_watermark_flat[index % len(encrypted_watermark_flat)]
+                    dct_block[x, y] /= (1.0 + alpha)
+                    index+=1
+
+        # 逆DCT变换
+        idct_block = idct_2d(dct_block)
+        Y[i:i+block_size, j:j+block_size] = idct_block
+
+end_time_part2 = time.time()
+print("第二部分运行时间：", end_time_part2 - end_time_part1, "秒")
+
+# 合并处理后的Y通道和原始的U、V通道，并保存含水印图像
+merged_yuv_image = cv2.merge([Y, U, V])
+reconstructed_bgr_image = cv2.cvtColor(merged_yuv_image, cv2.COLOR_YUV2BGR)  # 修正颜色转换
+cv2.imwrite('embedded_image.jpg', reconstructed_bgr_image)  # 保存含水印图像
+
+
+# 计算并保存差异图像
+difference_image = cv2.absdiff(bgr_image, reconstructed_bgr_image)
+cv2.imwrite('difference_image.jpg', difference_image)
+
+total_time = time.time() - start_time
+print("总运行时间：", total_time, "秒")
+'''
